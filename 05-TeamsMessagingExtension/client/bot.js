2eda536e6345328b89489a5e1ea9299286cfdc13
rabwill <rabiawilliams@gmail.com>
2022-01-08 12:07:15 +1000

    adaptivecard templating - withstand issue on preview

--- 05-TeamsMessagingExtension/client/bot.js
+++ 05-TeamsMessagingExtension/client/bot.js
@@ -1,8 +1,12 @@
 import { TeamsActivityHandler, CardFactory } from 'botbuilder';
 import { getProductByName } from '../server/northwindDataService.js';
-
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const pdtCardPayload =require('../client/cards/productCard.json');
+import * as ACData from "adaptivecards-templating";
+import * as AdaptiveCards from "adaptivecards";
 export class StockManagerBot extends TeamsActivityHandler {
-    async handleTeamsMessagingExtensionQuery(context, query) {
+    async handleTeamsMessagingExtensionQuery(context, query){
         const { name, value } = query.parameters[0];
         if (name !== 'productName') {
             return;
@@ -14,7 +18,8 @@ export class StockManagerBot extends TeamsActivityHandler {
         for (const pdt of products) {
             const heroCard = CardFactory.heroCard(pdt.productName);
             const preview = CardFactory.heroCard(pdt.productName);
-            preview.content.tap = { type: 'invoke', value: { name: pdt.productName } };
+            preview.content.tap = { type: 'invoke', value: { name: pdt.productName,
+            id:pdt.productId } };
             const attachment = { ...heroCard, preview };
             attachments.push(attachment);
         }
@@ -31,15 +36,30 @@ export class StockManagerBot extends TeamsActivityHandler {
 
     }
     async handleTeamsMessagingExtensionSelectItem(context, pdt) {
+        const preview = CardFactory.heroCard(pdt.name);
+        var template = new ACData.Template(pdtCardPayload);
+      
+        var card = template.expand({
+            $root: {
+                pdt
+            }
+        });
+        var adaptiveCard = new AdaptiveCards.AdaptiveCard();
+        adaptiveCard.parse(card);
+        const adaptive = CardFactory.adaptiveCard(card);
+        const attachment = { ...adaptive, preview };
         return {
             composeExtension: {
                 type: 'result',
                 attachmentLayout: 'list',
-                attachments: [CardFactory.heroCard(pdt.name)]
-            }
+                attachments: [attachment]
+            },
         };
-
-    }
+    }   
+    
 }
+    
+    
+
 
 
